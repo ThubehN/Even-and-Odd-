57d682c0e31424b8ac70947a6e4d17eaa0656267
ThubehN <82512827+ThubehN@users.noreply.github.com>
2021-06-18 06:08:00 +0200

    added html and javaScript

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+function evenOrOddNumber (num){
+    if (num % 2 == 0){
+      console.log("This is a even number!");
+    } else
+      console.log ("This is a odd number!");
+  }
+  
+  evenOrOddNumber (3);
+  evenOrOddNumber (100);
